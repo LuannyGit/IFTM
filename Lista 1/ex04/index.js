18ee4e1c40ef1c81d5a8204709a7e0ab98c128da
Luanny Victoria Araujo saito <luanny22saito@gmail.com>
2026-08-07 22:21:01 -0300

    Delete Lista 1 directory

--- Lista 1/ex04/index.js
+++
@@ -1,8 +0,0 @@
-nota1 = prompt(`Informe nota do 1° bimestre: `)
-nota2 = prompt(`Informe nota do 2° bimestre: `)
-resu = parseFloat(nota1) + parseFloat(nota2)
-if(resu >= 60){
-    alert("Aluno aprovado")
-}else{
-    alert(`Aluno reprovado\nFaltando ${60 - resu} pontos para ter sido aprovado`)
-}
